28154e54a351f1b088201bb74f09dbc14975b2e9
Susam Pal <susam@susam.net>
2026-01-18 00:18:27 +0000

    Add serial number to every row

--- backlog.sh
+++ backlog.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
 touch backlog.md
-echo '| COMMENT                                       | URL | D | I | REM |' > tmp.md
-echo '|-----------------------------------------------|-----|---|---|-----|' >> tmp.md
+echo '| #    | COMMENT                                       | URL                                 | D | I | REM             |' > tmp.md
+echo '|------|-----------------------------------------------|-------------------------------------|---|---|-----------------|' >> tmp.md
+#    '| 0001 | https://news.ycombinator.com/item?id=46618723 | https://jgc.org/                    | Y | Y |                 |'
+                                             sfmt='| %s | %s |                                     |   |   |                 |'
+n=1
 while read -r id
 do
+    sno=$(printf '%04d' "$n")
     if grep -q "?id=$id " backlog.md
     then
         echo "Copying existing comment $id"
-        grep "?id=$id" backlog.md >> tmp.md
+        data=$(grep "?id=$id" backlog.md | cut -d'|' -f3- | sed 's/^ *//g')
+        echo "| $sno | $data" >> tmp.md
     else
         echo "Adding new comment $id"
         url="https://news.ycombinator.com/item?id=$id"
-        echo "| $url |     |   |   |     |" >> tmp.md
+        printf "$sfmt" "$sno" "$url" >> tmp.md
+        echo >> tmp.md
     fi
+    n=$(( $n + 1 ))
 done < ids.txt
 mv tmp.md backlog.md
